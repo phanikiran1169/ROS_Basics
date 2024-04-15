02e69e9581118eb80eb1646ca7c26ecd97ac6380
Phani Kiran <phanikiran1169@gmail.com>
2024-04-16 00:17:48 +0530

    ROS2 - Added simple dynamic broadcaster

--- ROS2/bumperbot_cpp_example/include/bumperbot_cpp_example/simple_tf_kinematics.hpp
+++ ROS2/bumperbot_cpp_example/include/bumperbot_cpp_example/simple_tf_kinematics.hpp
@@ -3,6 +3,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 #include <memory>
 
@@ -11,7 +12,17 @@ class SimpleTfKinematics : public rclcpp::Node
 public:
     SimpleTfKinematics(const std::string& name);
 private:
+    void timerCallback();
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
     std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_broadcaster_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> dynamic_tf_broadcaster_;
+    
     geometry_msgs::msg::TransformStamped static_transform_stamped_;
+    geometry_msgs::msg::TransformStamped dynamic_transform_stamped_;
+    
+    double last_x_;
+    double x_increment_;
 
 };
