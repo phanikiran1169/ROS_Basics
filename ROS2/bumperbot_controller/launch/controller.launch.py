52ae7e01aeb10873bda0fa3283d170a7687d25f9
Phani Kiran <phanikiran1169@gmail.com>
2024-04-15 20:55:53 +0530

    ROS2 - Differential kinematics and simple velocity controller

--- ROS2/bumperbot_controller/launch/controller.launch.py
+++ ROS2/bumperbot_controller/launch/controller.launch.py
@@ -1,8 +1,24 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.033",
+    )
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation",
+        default_value="0.17",
+    )
+
+
+
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -23,9 +39,20 @@ def generate_launch_description():
         ]
     )
 
+    bumperbot_controller = Node(
+        package="bumperbot_controller",
+        executable="simple_controller",
+        parameters=[
+            {"wheel_radius": wheel_radius,
+                "wheel_separation": wheel_separation}]
+    )
+
     return LaunchDescription(
         [
+            wheel_radius_arg,
+            wheel_separation_arg,
             joint_state_broadcaster_spawner,
             simple_controller,
+            bumperbot_controller
         ]
     )
