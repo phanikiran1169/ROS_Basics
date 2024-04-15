8bf3f3fde76b9fc8596b81da175b49babdcfd31a
Phani Kiran <phanikiran1169@gmail.com>
2024-04-15 22:12:42 +0530

    ROS2 - Added diff_drive_controller

--- ROS2/bumperbot_controller/launch/controller.launch.py
+++ ROS2/bumperbot_controller/launch/controller.launch.py
@@ -1,11 +1,16 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import UnlessCondition, IfCondition
 
 
 def generate_launch_description():
-
+    
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True",
+    )
     wheel_radius_arg = DeclareLaunchArgument(
         "wheel_radius",
         default_value="0.033",
@@ -14,9 +19,8 @@ def generate_launch_description():
         "wheel_separation",
         default_value="0.17",
     )
-
-
-
+    
+    use_simple_controller = LaunchConfiguration("use_simple_controller")
     wheel_radius = LaunchConfiguration("wheel_radius")
     wheel_separation = LaunchConfiguration("wheel_separation")
 
@@ -30,29 +34,44 @@ def generate_launch_description():
         ],
     )
 
-    simple_controller = Node(
+    wheel_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["simple_velocity_controller",
-                "--controller-manager",
-                "/controller_manager"
-        ]
+        arguments=["bumperbot_controller", 
+                   "--controller-manager", 
+                   "/controller_manager"
+        ],
+        condition=UnlessCondition(use_simple_controller),
     )
 
-    bumperbot_controller = Node(
-        package="bumperbot_controller",
-        executable="simple_controller",
-        parameters=[
-            {"wheel_radius": wheel_radius,
-                "wheel_separation": wheel_separation}]
+    simple_controller = GroupAction(
+        condition=IfCondition(use_simple_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["simple_velocity_controller", 
+                           "--controller-manager", 
+                           "/controller_manager"
+                ]
+            ),
+            Node(
+                package="bumperbot_controller",
+                executable="simple_controller",
+                parameters=[
+                    {"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation}],
+            ),
+        ]
     )
 
     return LaunchDescription(
         [
+            use_simple_controller_arg,
             wheel_radius_arg,
             wheel_separation_arg,
             joint_state_broadcaster_spawner,
+            wheel_controller_spawner,
             simple_controller,
-            bumperbot_controller
         ]
     )
