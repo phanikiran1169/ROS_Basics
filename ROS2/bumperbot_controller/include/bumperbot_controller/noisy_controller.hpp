2810cb06d8ba20da4bcf0a4cd0a9ae12f12d02b2
Phani Kiran <phanikiran1169@gmail.com>
2024-04-28 17:12:05 +0530

    ROS2 - Added noise to odometry data

---
+++ ROS2/bumperbot_controller/include/bumperbot_controller/noisy_controller.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+
+class NoisyController : public rclcpp::Node
+{
+public:
+    NoisyController(const std::string& name);
+
+private:
+    void jointCallback(const sensor_msgs::msg::JointState &state);
+    
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+
+    double wheel_radius_;
+    double wheel_separation_;
+    
+    double right_wheel_prev_pos_;
+    double left_wheel_prev_pos_;
+    rclcpp::Time prev_time_;
+    double x_;
+    double y_;
+    double theta_;
+
+    nav_msgs::msg::Odometry odom_msg_;
+
+    // TF
+    std::unique_ptr<tf2_ros::TransformBroadcaster> transform_broadcaster_;
+    geometry_msgs::msg::TransformStamped transform_stamped_;
+    
+};
